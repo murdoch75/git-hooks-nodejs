a3283575567896335fb662baa7f7a4b314a48ce7
Ashish Deshpande <iashish.deshpande@gmail.com>
2023-01-01 17:31:32 +0530

    Two sum and mult modules with tests

---
+++ tests/mult.test.js
@@ -0,0 +1,5 @@
+const mult = require("../src/mult");
+it("should return correct multiplication", () => {
+  const result = mult(2, 5);
+  expect(result).toBe(10);
+});
---
+++ tests/sum.test.js
@@ -0,0 +1,5 @@
+const sum = require("../src/sum");
+it("should return correct sum", () => {
+  const result = sum(2, 5);
+  expect(result).toBe(7);
+});
