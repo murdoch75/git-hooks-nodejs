a009b6664de89d1bdbf4e735b349f5323639dc1e
Ashish Deshpande <iashish.deshpande@gmail.com>
2023-03-05 19:15:54 +0530

    Changed jest configuration, modified test files

--- src/sum.js
+++ src/sum.js
@@ -1,2 +1,2 @@
-const sum = (a, b) => a + b + b;
+const sum = (a, b) => a + b;
 module.exports = sum;
--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,5 +1,6 @@
 const sum = require("../src/sum");
+
 it("should return correct sum", () => {
-  const result = sum(2, 5);
-  expect(result).toBe(7);
-});
+  const result = sum(2, 6);
+  expect(result).toEqual(8);
+}, 70000);
